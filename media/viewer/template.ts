718e9edb58bc2bbae43cfde3f3316552dbf861d8
Nepomuk Wolf <nepomuk.wolf@tum.de>
2026-08-19 12:49:57 +0200

    change preview hover tooltip text

--- media/viewer/template.ts
+++ media/viewer/template.ts
@@ -27,7 +27,7 @@ export function viewerTemplate(state: ViewerTemplateState, actions: ViewerTempla
       <div
         class="hud-info"
         title=${state.hudInfo
-          ? "Descendants are included because ifc.viewer.includeChildren is enabled. Disable that setting to preview only the selected element."
+          ? "Includes descendants. Disable ifc.viewer.includeChildren to preview only the selected element."
           : ""}
       >
         ${state.hudInfo}
